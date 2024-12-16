34da628ad572563715a532eb4d144f80f198aab7
mingqiang <1165109730@qq.com>
2024-12-16 11:49:34 +0800

    first

--- conf.php
+++ conf.php
@@ -6,11 +6,11 @@ define('DEBUG',false);
 
 
 //============DB Settings
-define('PEC_DB_HOST','localhost');
-define('PEC_DB_USER','root');
-define('PEC_DB_PASS','root');
+define('PEC_DB_HOST','8.217.185.38');
+define('PEC_DB_USER','cal_educn_com_au');
+define('PEC_DB_PASS','rmCQXQrFGCeEcwAp');
 define('PEC_DB_TYPE','mysql');
-define('PEC_DB_NAME','php_event_calendar');
+define('PEC_DB_NAME','cal_educn_com_au');
 define('PEC_DB_CHARSET','');
 /******** DO NOT MODIFY ***********/
 require_once('pec.php');
